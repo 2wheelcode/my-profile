850f3d4671d6e901badd4f690ce6ce1f0719e593
2wheelcode <bentleyhamm@gmail.com>
2022-05-20 14:16:32 -0600

    Localstorage hold theme set

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-console.log("its working")
+console.log("its working");
+
+let theme = localStorage.getItem('theme');
+if(theme === null) {
+    setTheme('light');
+} else {
+    setTheme(theme);
+}
 
 let themeDots =document.getElementsByClassName('theme-dot');
 
@@ -23,4 +30,6 @@ function setTheme(mode) {
     if(mode == 'purple') {
         document.getElementById('theme-style').href = 'styles/purple.css'
     }
+
+    localStorage.setItem('theme', mode)
 }
