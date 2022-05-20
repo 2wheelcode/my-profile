a2ad9b464994a369fa82a4380f5fd39472e61287
2wheelcode <bentleyhamm@gmail.com>
2022-05-20 14:08:35 -0600

    Theme switch success, now smooth transition

--- script.js
+++ script.js
@@ -4,7 +4,23 @@ let themeDots =document.getElementsByClassName('theme-dot');
 
 for (var i=0; themeDots.length > i; i++) {
     themeDots[i].addEventListener('click', function(){
-        let mode = this.dataset.mode
+        let mode = this.dataset.mode;
         console.log('option clicked:', mode);
+        setTheme(mode);
     })
+}
+
+function setTheme(mode) {
+    if(mode == 'light') {
+        document.getElementById('theme-style').href = 'styles/default.css'
+    }
+    if(mode == 'blue') {
+        document.getElementById('theme-style').href = 'styles/blue.css'
+    }
+    if(mode == 'green') {
+        document.getElementById('theme-style').href = 'styles/green.css'
+    }
+    if(mode == 'purple') {
+        document.getElementById('theme-style').href = 'styles/purple.css'
+    }
 }
